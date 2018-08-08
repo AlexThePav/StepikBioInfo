2dc0ebe3b11789deec8f0f46bbe5af67c520f477
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2018-08-09 00:21:13 +0300

    adjacents to nodes

--- Stepik/2 Genome Sequencing/Week 1/DeBruijn.py
+++ Stepik/2 Genome Sequencing/Week 1/DeBruijn.py
@@ -5,11 +5,11 @@ from Overlap_graph import overlap_graph
 # Output: DeBruijnk(Text), in the form of an adjacency list.
 def debruijn(k, text):
     kmer_list = composition(k-1, text)
-    adjacents = overlap_graph(kmer_list)
+    nodes = overlap_graph(kmer_list)
 
 
 
-    return adjacents
+    return nodes
 
 
 print(debruijn(4, "AAGATTCTCTAAGA"))
